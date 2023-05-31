631905e3714afc5453ee11062068d4f34a05118f
MSiesto <miguelsiesto@gmail.com>
2023-05-31 21:18:38 +0200

    Creating UserListPage&&Component

---
+++ src/components/UserList/UserList.jsx
@@ -0,0 +1,9 @@
+
+function UserList() {
+    const [users, setUsers] = useState()
+    return (
+        <h1>UserList</h1>
+    )
+}
+
+export default UserList
---
+++ src/pages/UserListPage/UserListPage.jsx
@@ -0,0 +1,10 @@
+import UserList from "../../components/UserList/UserList"
+import { useState, useContext, useEffect } from "react"
+
+function UserListPage() {
+    return (
+        <UserList />
+    )
+}
+
+export default UserListPage
