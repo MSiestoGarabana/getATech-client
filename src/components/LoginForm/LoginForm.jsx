5bb4061ba6ffc189f49c822bec3a1f808a47ff88
MSiesto <miguelsiesto@gmail.com>
2024-01-19 00:41:00 +0100

    creating authenticateUser & storetoken methods

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,65 +1,69 @@
-import { useContext, useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import authService from './../../services/auth.services'
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../contexts/auth.contexts'
+import { useContext, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import authService from "./../../services/auth.services";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth.contexts";
 
-import './LoginForm.css'
+import "./LoginForm.css";
 
 function LoginForm() {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [loginData, setLoginData] = useState({
-        email: '',
-        password: ''
-    })
+  const navigate = useNavigate();
+  const { authenticateUser, storeToken } = useContext(AuthContext);
 
-    const navigate = useNavigate()
-    const { authenticateUser, storeToken } = useContext(AuthContext)
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
 
-    const handleInputChange = e => {
-        const { value, name } = e.target
-        setLoginData({ ...loginData, [name]: value })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = e => {
+    authService
+      .login(loginData)
+      .then(({ data }) => {
+        storeToken(data.authToken);
+        authenticateUser();
+        navigate("/homepage");
+      })
+      .catch((err) => console.log(err));
+  };
 
-        e.preventDefault()
+  const { password, username } = loginData;
 
-        authService
-            .login(loginData)
-            .then(({ data }) => {
-                storeToken(data.authToken)
-                authenticateUser()
-                navigate('/homepage')
-            })
-            .catch(err => console.log(err))
-    }
+  return (
+    <Form onSubmit={handleSubmit} id="loginForm__container--body">
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label id="loginForm__text">User Name</Form.Label>
+        <Form.Control
+          type="username"
+          value={username}
+          onChange={handleInputChange}
+          name="username"
+        />
+      </Form.Group>
 
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label id="loginForm__text">Contraseña</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={handleInputChange}
+          name="password"
+        />
+      </Form.Group>
 
-
-
-    const { password, email } = loginData
-
-    return (
-
-        <Form onSubmit={handleSubmit} id="loginForm__container--body">
-
-            <Form.Group className="mb-3" controlId="email">
-                <Form.Label id="loginForm__text">Email</Form.Label>
-                <Form.Control type="email" value={email} onChange={handleInputChange} name="email" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label id="loginForm__text">Contraseña</Form.Label>
-                <Form.Control type="password" value={password} onChange={handleInputChange} name="password" />
-            </Form.Group>
-
-            <div className="d-grid">
-                <Button variant="primary" type="submit">Acceder</Button>
-            </div>
-
-        </Form>
-    )
+      <div className="d-grid">
+        <Button variant="primary" type="submit">
+          Acceder
+        </Button>
+      </div>
+    </Form>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
--- src/utils/get-session-data.js
+++ src/utils/get-session-data.js
@@ -1,17 +1,20 @@
-import { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../contexts/auth.contexts';
-import userService from '../services/user.services';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../contexts/auth.contexts";
+import userService from "../services/user.services";
 
 export function useSessionData() {
-    const { user } = useContext(AuthContext)
-    const [sessionData, setSessionData] = useState({})
-  
-        useEffect(() => {
-            user && userService
-                .getUserById(user._id)
-                .then(({ data }) => { setSessionData(data) })
-                .catch(err => console.log(err))
-        }, [user])
+  const { user } = useContext(AuthContext);
+  const [sessionData, setSessionData] = useState({});
 
-    return sessionData
+  useEffect(() => {
+    user &&
+      userService
+        .getUserById(user._id)
+        .then(({ data }) => {
+          setSessionData(data);
+        })
+        .catch((err) => console.log(err));
+  }, [user]);
+
+  return sessionData;
 }
